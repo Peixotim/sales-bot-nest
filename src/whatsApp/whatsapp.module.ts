ab41e3e6bd4a1f7a4b8e9cc5515a2a78c6711e2f
Peixotim <pedropeixotovz@gmail.com>
2025-11-25 17:07:01 -0300

    feat: Configura o WhatsAppModule

--- src/whatsApp/whatsapp.module.ts
+++ src/whatsApp/whatsapp.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { WhatsAppController } from './whatsapp.controller';
 import { WhatsAppService } from './whatsapp.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { WhatsAppSession } from './entity/whatsapp-session.entity';
+import { WhatsAppAuthService } from './providers/whatsapp-auth.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([WhatsAppSession])],
   controllers: [WhatsAppController],
-  providers: [WhatsAppService],
+  providers: [WhatsAppService, WhatsAppAuthService],
 })
 export class WhatsAppModule {}
