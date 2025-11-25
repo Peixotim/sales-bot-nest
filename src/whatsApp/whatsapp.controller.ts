9ab47380a376416633445de3f523cb912573a7b8
Peixotim <pedropeixotovz@gmail.com>
2025-11-25 17:06:52 -0300

    feat: Adiciona os controllers status , qr

--- src/whatsApp/whatsapp.controller.ts
+++ src/whatsApp/whatsapp.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { WhatsAppService } from './whatsapp.service';
 
 @Controller('whatsapp')
-export class WhatsAppController {}
+export class WhatsAppController {
+  constructor(private readonly whatsAppService: WhatsAppService) {}
+
+  @Get('status')
+  public getStatus() {
+    return this.whatsAppService.getStatus();
+  }
+
+  @Get('qr')
+  public getQrCode() {
+    return this.whatsAppService.getQrCode();
+  }
+}
