8fdfff6e614bc0da5c469a6fafea199612a3db91
Peixotim <pedropeixotovz@gmail.com>
2025-11-25 17:03:57 -0300

    feat: Faz as configurações do swagger , do pino, pipes e etc

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { Logger } from 'nestjs-pino';
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    bufferLogs: true, //Habilita bufferLogs
+  });
+
+  app.useLogger(app.get(Logger)); //Habilita o pino
+
   await app.listen(process.env.PORT ?? 8080);
 
   app.useGlobalPipes(
