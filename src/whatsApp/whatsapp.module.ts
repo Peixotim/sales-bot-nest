f8c0aeaddc641420b9edc22ba184f51b2888c1dc
Peixotim <pedropeixotovz@gmail.com>
2025-11-25 21:59:33 -0300

    feat: Adiciona a importacao do AiService

--- src/whatsApp/whatsapp.module.ts
+++ src/whatsApp/whatsapp.module.ts
@@ -4,10 +4,11 @@ import { WhatsAppService } from './whatsapp.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { WhatsAppSession } from './entity/whatsapp-session.entity';
 import { WhatsAppAuthService } from './providers/whatsapp-auth.service';
+import { AiService } from 'src/ai/ai.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([WhatsAppSession])],
   controllers: [WhatsAppController],
-  providers: [WhatsAppService, WhatsAppAuthService],
+  providers: [WhatsAppService, WhatsAppAuthService, AiService],
 })
 export class WhatsAppModule {}
