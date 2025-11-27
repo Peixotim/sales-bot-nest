3979ba5874a7ace53e80a15ac4f6c3021f7e703d
Peixotim <pedropeixotovz@gmail.com>
2025-11-27 16:40:44 -0300

    feat: Cria todos os serviços do auth

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,62 @@
-import { Injectable } from '@nestjs/common';
-
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { ConsultantService } from 'src/consultant/consultant.service';
+import * as bcrypt from 'bcrypt';
+import { LoginDto } from './DTOs/login.dto';
+import { JwtPayload } from 'src/interfaces/JwtPayload';
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private consultantService: ConsultantService,
+    private jwtService: JwtService,
+  ) {}
+
+  public async validateUser(email: string, pass: string) {
+    const user = await this.consultantService.findByEmailForLogin(email);
+
+    const passwordCompare = await bcrypt.compare(pass, user!.password);
+
+    if (!user) {
+      throw new NotFoundException(
+        'Erro : Não foi possivel achar uma conta com este email , verifique porfavor suas credenciais e tente logar novamente !',
+      );
+    }
+
+    if (passwordCompare === false) {
+      throw new BadRequestException('Erro : Credenciais incorretas !');
+    }
+
+    if (user && passwordCompare) {
+      return user;
+    }
+
+    return null;
+  }
+
+  public async login(login: LoginDto) {
+    const user = await this.validateUser(login.email, login.password);
+
+    if (!user) {
+      throw new UnauthorizedException('Email ou senha incorretos');
+    }
+
+    const payload: JwtPayload = {
+      email: user.email,
+      sub: user.number,
+      name: user.name,
+    };
+    return {
+      access_token: this.jwtService.sign(payload),
+      user: {
+        name: user.name,
+        email: user.email,
+        number: user.number,
+      },
+    };
+  }
+}
