e2608ded6e997adc7fab2999962d9dca48343dbf
Peixotim <pedropeixotovz@gmail.com>
2025-11-27 16:45:47 -0300

    feat: Adiciona a importacao do WhatsAppGateway , e do JwtModule alem de 'configurar-los'

--- src/whatsApp/whatsapp.module.ts
+++ src/whatsApp/whatsapp.module.ts
@@ -7,13 +7,17 @@ import { WhatsAppAuthService } from './providers/whatsapp-auth.service';
 import { AiService } from 'src/ai/ai.service';
 import { ChatHistory } from 'src/ai/entities/chat-history.entity';
 import { ContactsModule } from 'src/contacts/contacts.module';
+import { WhatsAppGateway } from './whatsapp.gateway';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([WhatsAppSession, ChatHistory]),
     ContactsModule,
+    JwtModule.register({}),
   ],
   controllers: [WhatsAppController],
-  providers: [WhatsAppService, WhatsAppAuthService, AiService],
+  providers: [WhatsAppService, WhatsAppAuthService, AiService, WhatsAppGateway],
+  exports: [],
 })
 export class WhatsAppModule {}
