9fe28cdc98244bd79a1a52c3185864c1e4d4328f
Peixotim <pedropeixotovz@gmail.com>
2025-11-27 16:40:11 -0300

    feat: Cria as rotas do de login,register e me

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,41 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Post,
+  Req,
+  UseGuards,
+} from '@nestjs/common';
+import { ConsultantService } from 'src/consultant/consultant.service';
+import { RegisterConsultantDTO } from 'src/consultant/DTOs/consultant-register.dto';
+import { AuthService } from './auth.service';
+import { LoginDto } from './DTOs/login.dto';
+import { AuthGuard } from '@nestjs/passport';
+import type { RequestWithUser } from 'src/interfaces/request-with-user.interface';
 
-@Controller()
-export class AuthController {}
+@Controller('auth')
+export class AuthController {
+  constructor(
+    private readonly consultantService: ConsultantService,
+    private readonly authService: AuthService,
+  ) {}
+
+  @Post('register-consultant')
+  public async registerConsultant(@Body() request: RegisterConsultantDTO) {
+    return await this.consultantService.register(request);
+  }
+
+  @Post('login')
+  @HttpCode(HttpStatus.OK)
+  async login(@Body() loginDto: LoginDto) {
+    return this.authService.login(loginDto);
+  }
+
+  @UseGuards(AuthGuard('jwt'))
+  @Get('me')
+  getProfile(@Req() req: RequestWithUser) {
+    return req.user;
+  }
+}
