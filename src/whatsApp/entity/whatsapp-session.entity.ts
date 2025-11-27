1ba64ba660759fc8756190779ae500876249841c
Peixotim <pedropeixotovz@gmail.com>
2025-11-27 16:47:54 -0300

    feat: Adiciona o novo campo para reconhecer qual pessoa criou o qrcode

--- src/whatsApp/entity/whatsapp-session.entity.ts
+++ src/whatsApp/entity/whatsapp-session.entity.ts
@@ -3,6 +3,9 @@ import { Column, Entity, PrimaryColumn } from 'typeorm';
 @Entity({ name: 'whatsapp_sessions' })
 export class WhatsAppSession {
   @PrimaryColumn()
+  sessionId: string;
+
+  @PrimaryColumn()
   key: string;
 
   @Column({ type: 'text' })
